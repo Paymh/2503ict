195bbab3af95fdb5780daadc33161f27e09fc658
Lachlan H <lachlan.horsey@gmail.com>
2016-03-02 11:08:28 +0000

    Initial commit

---
+++ lab1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <meta charset="utf-8"> 
+2503ICT Web Programming – 7401ICT eService Technology Lab 1
+4
+ <title></title>
+ <meta name="description" content="">
+ <meta name="viewport" content="width=device-width">
+ </head>
+ <body>
+ <h1>Hello World</h1>
+ </body>
+</html> 
