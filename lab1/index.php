1e8dccee8153bf3e561e4e47bebb615f38a24979
Lachlan H <lachlan.horsey@gmail.com>
2016-03-08 03:07:22 +0000

    Final

--- lab1/index.php
+++ lab1/index.php
@@ -1,14 +1,47 @@
 <!DOCTYPE html>
 <html>
- <head>
- <meta charset="utf-8"> 
-2503ICT Web Programming – 7401ICT eService Technology Lab 1
-4
+
+<head>
+ <meta charset="utf-8"> 2503ICT Web Programming Lab 1
  <title></title>
  <meta name="description" content="">
  <meta name="viewport" content="width=device-width">
- </head>
- <body>
+</head>
+
+<body>
  <h1>Hello World</h1>
- </body>
-</html> 
+ <h2>Heading</h2>
+ <ul>
+  <li>
+   Sally
+  </li>
+  <li>
+   Fred
+  </li>
+ </ul>
+<table style="width:100%">
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td> 
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td> 
+    <td>94</td>
+  </tr>
+</table>
+ <img src="http://i.imgur.com/CxU6n.gif"></img>
+ <address>
+    You can contact author at <a href="http://www.somedomain.com/contact">www.somedomain.com</a>.<br>
+    If you see any bugs, please <a href="mailto:webmaster@somedomain.com">contact webmaster</a>.<br>
+    You may also want to visit us:<br>
+    Mozilla Foundation<br>
+    1981 Landings Drive<br>
+    Building K<br>
+    Mountain View, CA 94043-0801<br>
+    USA
+  </address>
+</body>
+
+</html>
